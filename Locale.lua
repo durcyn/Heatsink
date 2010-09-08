3044852b7c9d15de49e8415a77d8cde65c734191
durcyn <durcyn@gmail.com>
2010-09-08 10:27:23 -0500

    stop passing default args to non-enUS NewLocale calls, idiot

---
+++ Locale.lua
@@ -0,0 +1,35 @@
+local _G = getfenv(0)
+local LibStub = _G.LibStub
+
+local debug = false
+--@debug@
+debug = true
+--@end-debug@
+
+local L = LibStub("AceLocale-3.0"):NewLocale("Heatsink", "enUS", true, debug)
+--@localization(locale="enUS", format="lua_additive_table", same-key-is-true=true, handle-unlocalized="english")@
+
+L = LibStub("AceLocale-3.0"):NewLocale("Heatsink", "deDE")
+--@localization(locale="deDE", format="lua_additive_table", same-key-is-true=true, handle-unlocalized="english")@
+
+L = LibStub("AceLocale-3.0"):NewLocale("Heatsink", "esES")
+--@localization(locale="esES", format="lua_additive_table", same-key-is-true=true, handle-unlocalized="english")@
+
+L = LibStub("AceLocale-3.0"):NewLocale("Heatsink", "esMX")
+--@localization(locale="esMX", format="lua_additive_table", same-key-is-true=true, handle-unlocalized="english")@
+
+L = LibStub("AceLocale-3.0"):NewLocale("Heatsink", "frFR")
+--@localization(locale="frFR", format="lua_additive_table", same-key-is-true=true, handle-unlocalized="english")@
+
+L = LibStub("AceLocale-3.0"):NewLocale("Heatsink", "koKR")
+--@localization(locale="koKR", format="lua_additive_table", same-key-is-true=true, handle-unlocalized="english")@
+
+L = LibStub("AceLocale-3.0"):NewLocale("Heatsink", "ruRU")
+--@localization(locale="ruRU", format="lua_additive_table", same-key-is-true=true, handle-unlocalized="english")@
+
+L = LibStub("AceLocale-3.0"):NewLocale("Heatsink", "zhCN")
+--@localization(locale="zhCN", format="lua_additive_table", same-key-is-true=true, handle-unlocalized="english")@
+
+L = LibStub("AceLocale-3.0"):NewLocale("Heatsink", "zhTW")
+--@localization(locale="zhTW", format="lua_additive_table", same-key-is-true=true, handle-unlocalized="english")@
+
