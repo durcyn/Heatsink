2512f37e4c9ffc6ef34a47d700e421ff81979882
durcyn <durcyn@gmail.com>
2010-10-09 14:19:48 -0500

    clean up locales

--- Locale/deDE.lua
+++ Locale/deDE.lua
@@ -1,4 +1,5 @@
 local _G = getfenv(0)
 local LibStub = _G.LibStub
 local L = LibStub("AceLocale-3.0"):NewLocale("Heatsink", "deDE")
+if not L then return end
 --@localization(locale="deDE", format="lua_additive_table", same-key-is-true=true, handle-unlocalized="english")@
